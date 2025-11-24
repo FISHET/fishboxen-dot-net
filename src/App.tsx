cf01b54904301cbb6c9b00fe67dcd8efb4a6b892
fisher <fishboxen@gmail.com>
2025-11-24 14:41:38 +0000

    ✨ Add menu bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,55 @@
 import './App.css'
 import avatar from './assets/avatar.png'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faBluesky, faGithub, faLinkedinIn, faSpotify} from "@fortawesome/free-brands-svg-icons";
 
 function App() {
 
-  return (
-    <>
-      <div>
-        <a href="https://www.linkedin.com/in/tifisher" target="_blank">
-          <img src={avatar} className="logo" alt="My LinkedIn" />
-        </a>
-      </div>
-      <h1>TOM FISHER</h1>
-      <h2>Experienced SDET & Quality Coach</h2>
-    </>
-  )
+    return (
+        <>
+            <div className={"outer-grid-container"}>
+                <div>
+                    <a href="https://www.linkedin.com/in/tifisher" target="_blank">
+                        <img src={avatar} className="logo" alt="My LinkedIn"/>
+                    </a>
+                </div>
+                <h1>TOM FISHER</h1>
+                <h2>Experienced SDET & Quality Coach</h2>
+                <section className={"menu"}>
+                    <ul>
+                        <li>
+                            <a href={"https://www.linkedin.com/in/tifisher"}>
+                                <FontAwesomeIcon icon={faLinkedinIn} />
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href={"https://github.com/FISHET"}>
+                                <FontAwesomeIcon icon={faGithub} />
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href={"https://open.spotify.com/user/flying_hellfish"}>
+                                <FontAwesomeIcon icon={faSpotify}/>
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href={"https://bsky.app/profile/fishboxen.net"}>
+                                <FontAwesomeIcon icon={faBluesky} />
+                            </a>
+                        </li>
+
+
+                    </ul>
+
+                </section>
+            </div>
+
+        </>
+
+    )
 }
 
 export default App
