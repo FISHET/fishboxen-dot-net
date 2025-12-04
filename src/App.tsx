5043dfda28b55b085669a23d6e149194ca090a3b
fisher <fishboxen@gmail.com>
2025-12-04 10:54:14 +0000

    ✨ Add scrollToTop

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import HomePage from './pages/homepage/HomePage.tsx';
 import IntroPage from './pages/intropage/IntroPage.tsx';
+import ScrollToTop from './utils/ScrollToTop.tsx';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 
-export default function App() {
+export function App() {
     return (
         <>
             <HashRouter>
+                <ScrollToTop />
                 <Routes>
                     <Route path={'/'} element={<IntroPage />} />
                     <Route path="/home" index element={<HomePage />} />
